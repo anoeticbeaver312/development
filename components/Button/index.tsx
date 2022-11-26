5c9c3d7b030dc43b7fc33ab1873a4886cf704a0c
anoeticbeaver312 <galen_winsor+cs1300@brown.edu>
2022-11-26 15:54:18 -0500

    feat: button component

---
+++ components/Button/index.tsx
@@ -0,0 +1,19 @@
+import { JsxElement } from "typescript";
+import styles from "./Button.module.css";
+
+interface ButtonProps {
+  text: string;
+  type: "normal";
+  icon?: React.ReactElement;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <button className={`${styles.button} ${styles[props.type]}`}>
+      {props.icon}
+      {props.text}
+    </button>
+  )
+}
+
+export default Button;
