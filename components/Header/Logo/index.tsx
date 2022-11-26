4774e3328d34d9c0c2ed44982e6acf29b4a599fc
anoeticbeaver312 <galen_winsor+cs1300@brown.edu>
2022-11-26 17:12:20 -0500

    feat: header

---
+++ components/Header/Logo/index.tsx
@@ -0,0 +1,20 @@
+import Flex from "../../Flex";
+import styles from "./Logo.module.css";
+
+function Logo() {
+  return (
+    <Flex justifyContent="justifyBetween" styling="shadow-1">
+      <Flex alignItems="alignCenter">
+        <Flex styling={styles.iconContainer}>
+          <svg className={styles.svg} viewBox="0 0 100 100">
+            <circle cx="50" cy="50" r="25" fill="none" stroke="var(--orange-1)" strokeWidth="20" strokeDasharray="314.159265359 314.159265359"></circle>
+          </svg>
+        </Flex>
+        <h1 className={styles.h1}><span className={styles.u}>u</span>nit.</h1>
+      </Flex>
+      <p>Links</p>
+    </Flex>
+  )
+}
+
+export default Logo;
---
+++ components/Header/index.tsx
@@ -0,0 +1,9 @@
+import Logo from "./Logo";
+
+function Header() {
+  return (
+    <Logo />
+  )
+}
+
+export default Header;
